e3a66b3a593af76989d3c48e401f845f2abeb12e
lastvv <103041546+lastvv@users.noreply.github.com>
2022-07-10 16:58:51 +0300

    Update 1DniNedeli.py

--- 1DniNedeli.py
+++ 1DniNedeli.py
@@ -4,11 +4,26 @@
 # и проверяет, 
 # является ли этот день выходным
 
-n = int(input('Введите номер дня недели :'))
-while n < 6:
-    print('Будний День')
-    break
-else:
-    if(n == 6 or 7):
-        print('Ура,выходные!')
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("Ура Выходные")
+    elif 0 < num < 6:
+        print("Будний День")
+    else:
+        print("число вне пределов 7 дней")
+
+
+num = InputNumbers("Введите число: ")
+checkNumber(num)
     
